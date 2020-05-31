d5bb1feb480237d0b01e7a7ebba873082f953b36
denipersson <39971336+denipersson@users.noreply.github.com>
2020-05-31 19:28:04 +0200

    Everything needed in code for a functional tower
    FOR UNITY
    
    HOW TO USE:
    
    Add this script to your turret game object.
    
    The tower mesh needs to be divided in two parts: the stationary part and the moving part.
    The moving part is the one you assign as the "top" part in the inspector.
    
    You must also make an empty game object as a child of the turret, this game object needs a sphere collider (with the isTrigger checkbox checked). You then assign this as the "view Distance Sphere" in the inspector of the turret, and also add the TowerViewSphere.cs script. (this is what the turret uses to aim at and shoot enemies marked with the tag "enemy").
    
    You now have a working turret!

---
+++ Tower.cs
@@ -0,0 +1,174 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    public List<EnemyInfo> enemiesInRange = new List<EnemyInfo>();
+    [SerializeField]
+    private GameObject turretTopPart;
+    private Quaternion topStartRot;
+
+    [SerializeField]
+    private SphereCollider viewDistanceSphere;
+
+    [SerializeField]
+    private float turnSpeed = 3, aimForwardOffset = 2, viewDistance = 10, fireRate = 1, environmentalDestructionRadius = 2, extraAmmoSpeed = 0;
+    [SerializeField]
+    private EnemyInfo target;
+
+    [SerializeField]
+    private GameObject myAmmo;
+    [SerializeField]
+    private Transform ammoOriginPoint;
+    [SerializeField]
+    private bool canFire = true;
+
+    public void SetAmmo(GameObject ammo)
+    {
+        if (ammo.GetComponent<Ammo>())
+            myAmmo = ammo;
+    }
+    public GameObject GetAmmo()
+    {
+        return myAmmo;
+    }
+    public void SetFireRate(float rate)
+    {
+        if (rate > 0)
+            fireRate = rate;
+    }
+    public float GetEnvironmentalDestructionRadius()
+    {
+        return environmentalDestructionRadius;
+    }
+    void Clear()
+    {
+        for (int i = 0; i < enemiesInRange.Count; i++)
+        {
+            if (enemiesInRange[i] == null)
+                enemiesInRange.RemoveAt(i);
+        }
+    }
+
+    void Start()
+    {
+        if (!viewDistanceSphere)
+            viewDistanceSphere = GetComponent<SphereCollider>();
+
+        viewDistanceSphere.radius = viewDistance;
+
+        StartCoroutine(RazeSurroundings());
+
+        topStartRot = turretTopPart.transform.rotation;
+
+    }
+
+    void Update()
+    {
+        AimAtTarget();
+    }
+    void AssignTarget()
+    {
+        Clear();
+        if (!target)
+        {
+            if (enemiesInRange.Count > 0)
+            {
+                target = enemiesInRange[0];
+            }
+        }
+    }
+    void AimAtTarget()
+    {
+        AssignTarget();
+        if(target)
+        {
+            aimForwardOffset = target.Movement().GetAgent().velocity.magnitude * (Vector3.Distance(transform.position, target.transform.position) / 10);
+            Vector3 dir = transform.transform.position - (target.transform.position + target.transform.forward * -aimForwardOffset);
+            dir.y = 0;
+            Quaternion lookRot = Quaternion.LookRotation(-dir, Vector3.up);
+            RotateInDirection(lookRot);
+
+            if (canFire)
+                StartCoroutine(Fire());
+
+            if (Vector3.Distance(transform.position, target.transform.position) > viewDistance + 2.5f || !target.Health().IsAlive())
+                RemoveTarget();
+        }
+        else
+        {
+            RotateInDirection(topStartRot);
+        }
+    }
+    void RemoveTarget()
+    {
+        if(target)
+        {
+            if (enemiesInRange.Contains(target))
+                enemiesInRange.Remove(target);
+        }
+        if (enemiesInRange.Count > 0)
+            target = enemiesInRange[enemiesInRange.Count - 1]; // target the last enemy that entered range
+        else
+            target = null;
+    }
+    IEnumerator Fire()
+    {
+        canFire = false;
+
+        yield return new WaitForSeconds(fireRate);
+
+        if (!target)
+        {
+            canFire = true;
+            yield break;
+        }
+
+        this.GetComponent<AudioSource>().Play();
+        GameObject newAmmo = GameObject.Instantiate(myAmmo);
+        Ammo nA = newAmmo.GetComponent<Ammo>();
+        newAmmo.transform.position = ammoOriginPoint.transform.position;
+        newAmmo.transform.rotation = turretTopPart.transform.rotation;
+        nA.SetSpeed(nA.GetSpeed() + extraAmmoSpeed);
+        nA.SetDamage(nA.GetDamage() + (int)extraAmmoSpeed);
+
+        canFire = true;
+    }
+    IEnumerator RazeSurroundings()
+    {
+        viewDistanceSphere.enabled = false;
+        yield return new WaitForEndOfFrame();
+        viewDistanceSphere.enabled = true;
+    }
+    void RotateInDirection(Quaternion lookRotation)
+    {
+        turretTopPart.transform.rotation = Quaternion.Slerp(turretTopPart.transform.rotation, lookRotation, turnSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag.Equals("enemy", System.StringComparison.CurrentCultureIgnoreCase))
+        {
+            if (other.GetComponent<EnemyInfo>())
+                enemiesInRange.Add(other.GetComponent<EnemyInfo>());
+        }
+        if(other.tag.Equals("removable", System.StringComparison.CurrentCultureIgnoreCase))
+        {
+            if(Vector3.Distance(transform.position, other.transform.position) <= environmentalDestructionRadius)
+            {
+                Destroy(other.gameObject);
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag.Equals("enemy", System.StringComparison.CurrentCultureIgnoreCase))
+        {
+            if (other.GetComponent<EnemyInfo>())
+                enemiesInRange.Remove(other.GetComponent<EnemyInfo>());
+        }
+    }
+
+
+}
---
+++ TowerViewSphere.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerViewSphere : MonoBehaviour
+{
+    public Tower myTower;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!myTower)
+            myTower = GetComponentInParent<Tower>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("enemy", System.StringComparison.CurrentCultureIgnoreCase))
+        {
+            if (other.GetComponent<EnemyInfo>())
+                myTower.enemiesInRange.Add(other.GetComponent<EnemyInfo>());
+        }
+        if (other.tag.Equals("removable", System.StringComparison.CurrentCultureIgnoreCase))
+        {
+            if (Vector3.Distance(transform.position, other.transform.position) <= myTower.GetEnvironmentalDestructionRadius())
+            {
+                Destroy(other.gameObject);
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag.Equals("enemy", System.StringComparison.CurrentCultureIgnoreCase))
+        {
+            if (other.GetComponent<EnemyInfo>())
+                myTower.enemiesInRange.Remove(other.GetComponent<EnemyInfo>());
+        }
+    }
+}
